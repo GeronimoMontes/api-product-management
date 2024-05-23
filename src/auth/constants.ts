9999d1c95e15a06c2ee08ecf15a1ce2a8cf197fb
GeronimoMontes <geronimo@adroitoverseas.com>
2024-05-23 11:52:19 -0700

    add auth moule

---
+++ src/auth/constants.ts
@@ -0,0 +1,4 @@
+export const jwtConstants = {
+  secret:
+    'eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTcxNjQ4OTU2NSwiaWF0IjoxNzE2NDg5NTY1fQ.Oo6188Wry_L-m8B-nkZ7FqmyfYmBfE3AaWi-kANIjPY',
+};
